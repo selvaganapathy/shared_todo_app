6372732e548015432a33f70c9cbda8dc681ad40c
selvaganapathy <k.selvaganapathy@yahoo.com>
2014-06-07 13:54:20 +0530

    Shared Todo App

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,8 @@
+class TodosController < ApplicationController
+  def index
+  	@todo_items = Todo.all
+  end
+  def delete
+  	Todo.last.delete
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedTodoApp::Application.config.secret_token = 'bc4aa76ed73ba83faf53a71e64bf1a026bbc6816a51a8385f40b4eefa7a1fc8155daedb6bf7514e595d60133d0701b405e234a8576d83f05e5c4947a9817d59f'
